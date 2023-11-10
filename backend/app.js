51509fce364def13e35ebb60eefb3dd6e259e278
MYLAPTOP\MyLaptop <khaclam2409@gmail.com>
2023-11-10 13:16:21 +0700

    add xss and rate limit

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,22 @@ const userRouter = require("./routes/user.route");
 const pollRouter = require("./routes/poll.route");
 const voteRouter = require("./routes/vote.route");
 const optionRouter = require("./routes/option.route");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
 const AppError = require("./utils/appError");
 
 const app = express();
 app.use(express.json());
+
 app.use(cors());
 app.use(helmet());
+app.use(xss());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/polls", pollRouter);
